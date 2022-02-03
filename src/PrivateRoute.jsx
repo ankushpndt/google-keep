9d129b6ef58d254ddf6fc4bc770a20084c206704
Ankush <ankushpndt@gmail.com>
2022-02-03 22:49:37 +0530

    refactor: refactored code

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -4,5 +4,5 @@ import React from 'react';
 export const PrivateRoute = ({ children }) => {
   const { login } = useAuth();
 
-  return login ? children : <Navigate to='/login' />;
+  return login ? children : <Navigate to='/' />;
 };
